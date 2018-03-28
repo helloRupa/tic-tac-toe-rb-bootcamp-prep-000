a37ac7210183187b3dfd0ffa745cb0b6e43fa751
Rupinder Dhillon <rupa211984@gmail.com>
2018-03-28 06:33:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,7 +9,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
   ]
   
-def display_board (board)
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -17,15 +17,15 @@ def display_board (board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index (input)
+def input_to_index(input)
   input.to_i - 1
 end
 
-def move (board, idx, token)
+def move(board, idx, token)
     board[idx] = token
 end
 
-def position_taken? (board, idx)
+def position_taken?(board, idx)
   board[idx] == "X" || board[idx] == "O"
 end
 
@@ -53,4 +53,8 @@ def turn_count(board)
     end
   end
   count
+end
+
+def current_player(board)
+  
 end
